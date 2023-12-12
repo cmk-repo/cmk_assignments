5c8a4c8406a2884c21505ef6bd4e52b71337cd6c
cmk_wsl <contact@chandan.fyi>
2023-12-12 15:23:16 -0500

    Easy Done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,12 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false
+  }
+  const s1 = str1.toLowerCase().split('').sort().join('');
+  const s2 = str2.toLowerCase().split('').sort().join('');
+  return s1 === s2
 
 }
 
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,17 +4,36 @@
   transactions is an array where each
   Transaction - an object like 
         {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  var output = {} // create an object
+  for (let i = 0; i < transactions.length; i++) {
+    var yanked_tansaction = transactions[i]
+    if (output[yanked_tansaction.category]) {
+      output[yanked_tansaction.category] = output[yanked_tansaction.category] + yanked_tansaction.price
+    } else {
+      output[yanked_tansaction.category] = yanked_tansaction.price
+    }
+  }
+  // convert object to array 
+
+  var keys = Object.keys(output)
+  let final = [];
+  for (let i = 0; i < keys.length; i++) {
+    var yanked_category = keys[i];
+    final.push({ category: yanked_category, totalSpent: output[yanked_category] })
+  }
+  //console.log(transactions)
+  console.log(final)
+  return final;
 }
 
 module.exports = calculateTotalSpentByCategory;
